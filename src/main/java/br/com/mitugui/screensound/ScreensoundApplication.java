1bedbb338c7ca86472f407548f5470e5e6adb20d
mitugui <cristian.mitugui04@gmail.com>
2024-10-15 22:43:56 -0300

    feat: implement Main class with CommandLineRunner and interactive menu

--- src/main/java/br/com/mitugui/screensound/ScreensoundApplication.java
+++ src/main/java/br/com/mitugui/screensound/ScreensoundApplication.java
@@ -1,13 +1,20 @@
 package br.com.mitugui.screensound;
 
+import br.com.mitugui.screensound.main.Main;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreensoundApplication {
+public class ScreensoundApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreensoundApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+		main.displayMenu();
+	}
 }
---
+++ src/main/java/br/com/mitugui/screensound/main/Main.java
@@ -0,0 +1,67 @@
+package br.com.mitugui.screensound.main;
+
+import java.util.Scanner;
+
+public class Main {
+    Scanner scanner = new Scanner(System.in);
+
+    public void displayMenu() {
+        var option = -1;
+
+        while (option != 9) {
+            var menu = """
+                    *** Screen Sound Músicas ***                    
+                                        
+                    1- Cadastrar artistas
+                    2- Cadastrar músicas
+                    3- Listar músicas
+                    4- Buscar músicas por artistas
+                    5- Pesquisar dados sobre um artista
+                                    
+                    9 - Sair
+                    """;
+
+            System.out.println(menu);
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                    registerArtists();
+                    break;
+                case 2:
+                    registerSongs();
+                    break;
+                case 3:
+                    listSongs();
+                    break;
+                case 4:
+                    searchSongByArtist();
+                    break;
+                case 5:
+                    searchArtistData();
+                    break;
+                case 9:
+                    System.out.println("Encerrando a aplicação!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+    }
+
+    private void registerArtists() {
+    }
+
+    private void registerSongs() {
+    }
+
+    private void listSongs() {
+    }
+
+    private void searchSongByArtist() {
+    }
+
+    private void searchArtistData() {
+    }
+}
